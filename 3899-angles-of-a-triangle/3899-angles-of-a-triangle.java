3f8f64e2bc420f942b76009994ccd8a1a655cf26
Ritesh patidar <riteshpatidar2004feb@gmail.com>
2026-04-12 11:26:44 +0530

    Time: 4 ms (39.27%), Space: 49.4 MB (36.4%) - LeetHub

--- 3899-angles-of-a-triangle/3899-angles-of-a-triangle.java
+++ 3899-angles-of-a-triangle/3899-angles-of-a-triangle.java
@@ -1,17 +1,19 @@
 class Solution {
-    public double[] internalAngles(int[] sides) {
-         int a=sides[0],b=sides[1],c=sides[2];
-        if((a+b)<=c||(a+c)<=b||(c+b)<=a) return new double[0];
-        double[] nums=new double[3];
-        // if((a+b)>c&&(b+c)>a&&(c+a)>b){
-            double A=((b*b+c*c-a*a)/(2.0*b*c));
-            double B=((c*c+a*a-b*b)/(2.0*a*c));
-            double C=((b*b-c*c+a*a)/(2.0*b*a));
-            nums[0]=Math.acos(A)*180/Math.PI;
-             nums[1]=Math.acos(B)*180/Math.PI;
-             nums[2]=Math.acos(C)*180/Math.PI;
-       // }
-        Arrays.sort(nums);
-        return nums;
+    public double[] internalAngles(int[] arr) {
+        double[] ans = new double[3];
+        int a = arr[0];
+        int b = arr[1];
+        int c = arr[2];
+        if(a+b<=c) return new double[0];
+        else if(b+c<=a) return new double[0];
+        else if(c+a<=b) return new double[0];
+        double p = ((a*a)+(b*b)-(c*c))/(double)(2*a*b);
+        double q = ((c*c)+(b*b)-(a*a))/(double)(2*c*b);
+        double r = ((a*a)+(c*c)-(b*b))/(double)(2*a*c);
+        ans[0] = Math.acos(p)*(180)/Math.PI;
+        ans[1] = Math.acos(q)*(180)/Math.PI;
+        ans[2] = Math.acos(r)*(180)/Math.PI;
+        Arrays.sort(ans);
+        return ans;
     }
 }
