dabf56929e8587763b8cb1bac469338b1640e692
Ritesh patidar <riteshpatidar2004feb@gmail.com>
2026-04-12 11:21:18 +0530

    Time: 4 ms (39.27%), Space: 49.5 MB (36.4%) - LeetHub

--- 3899-angles-of-a-triangle/3899-angles-of-a-triangle.java
+++ 3899-angles-of-a-triangle/3899-angles-of-a-triangle.java
@@ -4,12 +4,12 @@ class Solution {
         if((a+b)<=c||(a+c)<=b||(c+b)<=a) return new double[0];
         double[] nums=new double[3];
         // if((a+b)>c&&(b+c)>a&&(c+a)>b){
-            double A=(b*b+c*c-a*a)/(2.0*b*c);
-            double B=(c*c+a*a-b*b)/(2.0*a*c);
-            double C=(b*b-c*c+a*a)/(2.0*b*a);
-            nums[0]=Math.toDegrees(Math.acos(A));
-             nums[1]=Math.toDegrees(Math.acos(B));
-             nums[2]=Math.toDegrees(Math.acos(C));
+            double A=((b*b+c*c-a*a)/(2.0*b*c));
+            double B=((c*c+a*a-b*b)/(2.0*a*c));
+            double C=((b*b-c*c+a*a)/(2.0*b*a));
+            nums[0]=Math.acos(A)*180/Math.PI;
+             nums[1]=Math.acos(B)*180/Math.PI;
+             nums[2]=Math.acos(C)*180/Math.PI;
        // }
         Arrays.sort(nums);
         return nums;
