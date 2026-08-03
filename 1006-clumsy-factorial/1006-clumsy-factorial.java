c9eb1cbd187589f25312a2968c2da8cf1023dd80
Ritesh patidar <riteshpatidar2004feb@gmail.com>
2026-08-03 13:23:36 +0530

    Time: 8 ms (55.13%), Space: 44.9 MB (55.13%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.java
+++ 1006-clumsy-factorial/1006-clumsy-factorial.java
@@ -1,35 +1,35 @@
 class Solution {
     public int clumsy(int n) {
         if(n == 1 || n == 2) return n;
-        Stack <Integer> st = new Stack <>();
+        ArrayList <Integer> list = new ArrayList <>();
 
         while( n > 0){
             if(n < 3){
-                st.push(n);
+                list.add(n);
                 break;
             }
 
             int n1 = n * (n - 1) / (n - 2);
-            st.push(n1);
+            list.add(n1);
             n = n - 3;
-            if(n != 0) st.push(n);
+            if(n != 0) list.add(n);
             n--;
         }
 
-    Stack <Integer> st1 = new Stack <>();
-     while(st.size() > 0){
-        st1.push(st.pop());
-     }
+    // Stack <Integer> st1 = new Stack <>();
+    //  while(st.size() > 0){
+    //     st1.push(st.pop());
+    //  }
         
-        int clumsy = st1.pop();
+        int clumsy = list.get(0);
         boolean ekbaar = true;
-        while(st1.size() > 0){
+        for(int i = 1; i < list.size(); i++){
             if(ekbaar){
-                clumsy += st1.pop();
+                clumsy += list.get(i);
                 ekbaar = false;
             }
             else {
-                clumsy -= st1.pop();
+                clumsy -= list.get(i);
                 ekbaar = true;
             }
         }
