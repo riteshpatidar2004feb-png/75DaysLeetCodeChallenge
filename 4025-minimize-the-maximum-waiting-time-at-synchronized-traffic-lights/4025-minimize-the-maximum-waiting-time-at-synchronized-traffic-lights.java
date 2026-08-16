cc54e7f811775116cd05c38c5625b4f2831785cb
Ritesh patidar <riteshpatidar2004feb@gmail.com>
2026-08-16 11:49:34 +0530

    Time: 3 ms (99.95%), Space: 121.9 MB (32.12%) - LeetHub

--- 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
@@ -1,11 +1,15 @@
 class Solution {
     public int minPenalty(int period, int[] lights, int[] arrivalTime) {
-        Arrays.sort(lights);
+        //Arrays.sort(lights);
+        int max_l= Integer.MIN_VALUE;
+        for(int i = 0; i < lights.length; i++) {
+            if(max_l < lights[i]) max_l = lights[i];
+        }
         int max = Integer.MIN_VALUE;
         for(int i = 0; i < arrivalTime.length; i++){
             int r = arrivalTime[i] % period;
             int wt = 0;
-            if(r >= lights[lights.length-1]){
+            if(r >= max_l){
                 wt = period - r;
             }
             if(wt > max) max = wt;
